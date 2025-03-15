2c267bd009e36ea077dfc5ec4dd196a1120d5f3f
Vinayak Dixit <vincidixit@gmail.com>
2025-03-15 19:39:25 +0530

    Boilerplate code along with a controller UserController

---
+++ JWTBearerAuthScheme/UserManagementService/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{username}")]
+        public IActionResult GetUserDetails(string username)
+        {
+            // Return dummy user details
+            return Ok(new { Name = "Miguel O'hara", Location = "Pune, MH, India" });
+        }
+    }
+}

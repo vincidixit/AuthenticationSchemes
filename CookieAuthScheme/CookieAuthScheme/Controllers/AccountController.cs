742ffbc3eb4a24a28292aa80d4a5f42d460519fa
Vinayak Dixit <vincidixit@gmail.com>
2025-03-16 11:23:37 +0530

    Boilerplate code with Account controller

---
+++ CookieAuthScheme/CookieAuthScheme/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using CookieAuthScheme.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieAuthScheme.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            if(loginModel.Username == "cisco.ramon" && loginModel.Password == "Admin@123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View();
+            }   
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CookieAuthScheme/CookieAuthScheme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookieAuthScheme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
